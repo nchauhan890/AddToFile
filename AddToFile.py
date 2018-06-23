79a12d5d5f2cdc600cfb0a1b6f348c718d3f6cfd
nchauhan890 <39838120+nchauhan890@users.noreply.github.com>
2018-06-23 19:28:27 +0100

    first working release

--- AddToFile.py
+++ AddToFile.py
@@ -5,62 +5,156 @@ import sublime_plugin
 
 
 class AddToCommand(sublime_plugin.TextCommand):
-    def __init__(self):
-        self.settings = sublime.load_settings('AddT.sublime-settings')
-        # load settings
+
+    def get_items(self):
+        return [view for view in sublime.active_window().views()
+                # if view.file_name()
+                if view != sublime.active_window().active_view()]
+
+    def get_contents(self, view):
+        length = 0
+        if isinstance(view, sublime.View):
+            items = []
+            for _ in range(3):
+                items.append(view.substr(view.line(length)))
+                # add the line contents to list
+                length += len(view.substr(view.line(length))) + 1
+                # record the cumulative character count to get each line
+
+        else:
+            items = ['' for _ in range(3)]
+            # return empty values if the view parameter isnt a view object
+        return items
+
+    def split_view_path(self, view):
+        try:
+            return list(os.path.split(view.file_name()))
+            # return a list containing [path, file]
+
+        except (AttributeError, TypeError):
+            return ['untitled', 'untitled']
+
+    def get_split_view_paths(self, views):
+        return [self.split_view_path(view) for view in views]
+        # returnlist = []
+        # for item in views:
+        #     returnlist.append(self.split_view_path(item))
+        # return returnlist
+
+    def get_view_paths(self, views):
+        return [view.file_name() for view in views]
 
     def on_done(self, val):
+        settings = sublime.load_settings('AddToFile.sublime-settings')
+        v = sublime.active_window().active_view()  # save the starting view
+
         if val == -1:
             return  # end if called with -1 value
         if self.items[val] == 'New File':
             # if the selected option is 'New File',
             # create a new file
             f = sublime.active_window().new_file()
+            self.items = self.get_items()
 
         else:  # selected item isn't a file so must be 'new file'
             f = self.items[val]
-        # sublime.active_window().focus_view(f)
-        f.run_command('insert_to_end',
-                      {"selection": [self.view.substr(s)
-                                     for s in self.view.sel()]})
-        # run insert command to add text
 
-        string = self.settings.get('status_message',
-                                   'Copied to {name}').format(
-                                   os.path.split(f.file_name())[1])
-        # set string with the name of the file that was copied to
-        if self.settings.get('show_status_message', False):
+        f.run_command('insert_to_end',
+                      {"lines": [self.view.substr(s)
+                                 for s in self.view.sel()]})
+
+        sublime.active_window().focus_view(v)  # use the starting view
+        # override when new file is created to stop focus switching
+
+        # run insert command to add text
+        string = settings.get('status_message')
+        string = string.format(name=self.split_view_path(f)[1],
+                               path=self.split_view_path(f)[0],
+                               sourcename=os.path.split(
+                               sublime.active_window().active_view().file_name())[1],
+                               sourcepath=os.path.split(
+                               sublime.active_window().active_view().file_name())[0])
+        # set string using template from settings, substituting
+        # values accordingly
+
+        if not settings.get('keep_focus', True):
+            sublime.active_window().focus_view(f)
+            # switch focus to destination file if specified in settings
+
+            if settings.get('scroll_view', False):
+                f.show(f.size(0))
+
+        if settings.get('show_status_message', False):
             # run status bar message command if value specified
             # in settings
             sublime.active_window().run_command('add_status_bar_msg',
                                                 {"msg": string})
 
     def run(self, edit):
+        settings = sublime.load_settings('AddToFile.sublime-settings')
+        # load settings file
+
         if not ''.join(self.view.substr(s) for s in self.view.sel()):
             return  # end if the selection is empty
 
-        self.items = [view
-                      for view in sublime.active_window().views()
-                      if view.file_name()
-                      if view != sublime.active_window().active_view()]
+        self.items = self.get_items()
         # add list of views, excluding the current view
 
-        if self.settings.get('add_to_suggest_new_file', False):
-            self.items.append('New File')
-            # add 'new file' option if specified settings
+        if settings.get('show_file_path', False):
+            self.paths = self.get_view_paths(self.items)
+            # get the 'unsplit' paths
 
-        # print(self.items)
-        # print(os.path.split(self.view.file_name())[1])
-        if self.settings.get('add_to_single_view', False) and len(self.items) == 1:
+        else:
+            self.paths = self.get_split_view_paths(self.items)
+        # make a list of the view paths
+
+        if settings.get('show_preview', False):
+            self.view_content = [self.get_contents(view)
+                                 for view in sublime.active_window().views()
+                                 if view != sublime.active_window().active_view()]
+            # get a list of starting content of the open views
+            # excluding the current view
+
+            self.popup = []
+
+            for path, content in zip(self.paths, self.view_content):
+                if settings.get('show_file_path', False):
+                    a = [path]  # add path to list since it's not a list
+                else:
+                    a = path[:]  # copy 'split' path list to temp variable
+                    a.pop(0)
+                a.extend(content)
+                self.popup.append(a)
+            # create popup list of strings which contains the file name and
+            # conten
+
+            if settings.get('add_to_suggest_new_file', False):
+                self.popup.append(['New File', '', '', ''])
+                # and 'New File' to popup list with blank lines
+
+        if settings.get('add_to_single_view', False) and len(self.items) == 1:
             self.on_done(0)
             # auto-run on_done if there's only 1 other view and if specified
             # in settings
-        else:
-            sublime.active_window().show_quick_panel([os.path.split(view.file_name())[1]
-                                                     for view in self.items],
+
+        elif settings.get('add_to_suggest_new_file', False):
+            self.items.append('New File')
+            # add 'new file' option if specified settings
+
+        if settings.get('show_preview', False):
+            sublime.active_window().show_quick_panel(self.popup,
                                                      self.on_done)
-            # show popup with the file names
-            # (and 'New File' if specified in settings)
+            # create popup with the *file name + preview* if specified in settings
+        elif settings.get('show_file_path', False):
+            sublime.active_window().show_quick_panel([path
+                                                      for path in self.paths],
+                                                     self.on_done)
+            # create popup with the *file paths* if specified in settings
+        else:
+            sublime.active_window().show_quick_panel([path[1]
+                                                      for path in self.paths],
+                                                     self.on_done)
+            # create popup with the *file names* if specified in settings
 
 
 class InsertToEndCommand(sublime_plugin.TextCommand):
